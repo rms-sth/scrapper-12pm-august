bbc4f841cf578c8410dd819e6a5ff0e31371cd48
Ramesh Pradhan <pyrameshpradhan@gmail.com>
2025-08-11 13:06:40 +0545

    Add git commands

--- scrape.py
+++ scrape.py
@@ -2,6 +2,13 @@
 # git config --global user.name "Ramesh Pradhan"
 # git config --global user.email "pyrameshpradhan@gmail.com"
 
+# git init
+# git status => if you want to check what are the status of files
+# git diff => if you want to check what are the changes
+# git add .
+# git commit -m "Your message"
+# copy paste git code from github
+
 
 # python -m pip install requests
 # => get data from web (html, json, xml)
